12dd1b0a03a55417b42dac6e959a19d499d5aa6f
DavidLuques <braian.luques2@gmail.com>
2023-03-14 19:18:37 -0300

    apps agregadas

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog', #we specified the blog app location here
+    'portfolio',#we specified the portfolio app location here
 ]
 
 MIDDLEWARE = [
