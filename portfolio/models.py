0705c897a59f4c234a5aa6026e2f22384dbe2bf4
DavidLuques <braian.luques2@gmail.com>
2023-03-14 20:24:51 -0300

    Warning

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+# from django.db.models.fields import CharField,files,URLField
+# from django.db.models.fields.files import ImageField
 # Create your models here.
+class project(models.Model):
+     title=models.CharField(max_length=30)
+     description=models.CharField(max_length=250)
+     image=models.ImageField(upload_to='portfolio/images')
+     url=models.URLField(blank=True)
